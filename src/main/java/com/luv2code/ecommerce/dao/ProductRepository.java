e82078ec4b9f2505664510767e86dea9d22901a6
Yazdan <1yazdanrafieemanesh@gmail.com>
2024-03-16 02:01:42 +0330

    complete search feature

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -9,5 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findByCategoryId(@Param("id")Long id , Pageable pageable);
+    Page<Product> findByCategoryId(@Param("id") Long id , Pageable pageable);
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
 }
